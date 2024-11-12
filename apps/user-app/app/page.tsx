516a00b4eb2e48252edf1eb25f49e32cee3e86e2
krish-98 <muralikrishnanm98@gmail.com>
2024-11-12 12:55:32 +0530

    minor changes2

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,7 +2,7 @@ import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import { authOptions } from './lib/auth'
 
-//Yeah
+//checking
 export default async function Page() {
   const session = await getServerSession(authOptions)
   if (session?.user) {
